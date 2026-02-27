083aa7c34469c9de6168f6e67a58d717d90f37bb
Bhoomika1177 <166717535+Bhoomika1177@users.noreply.github.com>
2026-02-28 02:02:09 +0530

    Overwrite remote with local code

---
+++ src/app/projects/tbc/page.jsx
@@ -0,0 +1,12 @@
+// projects/tbc/page.js
+"use client";
+import React from "react";
+
+export default function TbcPage() {
+  return (
+    <div>
+      <h1>TBC Project</h1>
+      <p>This is the TBC project page.</p>
+    </div>
+  );
+}
